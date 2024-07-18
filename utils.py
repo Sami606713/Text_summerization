3640e74123b058faa266f65ad69bb5f753f5eab5
samiullah <sami606713@gmail.com>
2024-07-19 01:19:01 +0500

    saperate key

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ summarize_api_key = os.getenv('SUMMARIZE_API_KEY')
 
 def summerize_text(payload):
     API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-    headers = {"Authorization":summarize_api_key}
+    headers = {"Authorization":'Bearer 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'}
 	
     response = requests.post(API_URL, headers=headers, json=payload)
 
