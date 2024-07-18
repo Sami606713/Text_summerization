3ffa4ba44f1c99a0b74dc7f50aa3ed0917991cba
samiullah <sami606713@gmail.com>
2024-07-19 01:23:19 +0500

    adjust the key

--- utils.py
+++ utils.py
@@ -1,11 +1,5 @@
 import requests
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# Access the environment variables
-summarize_api_key = os.getenv('SUMMARIZE_API_KEY')
 
 def summerize_text(payload):
     API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
